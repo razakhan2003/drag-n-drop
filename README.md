9a7c5fe3788a08b9fd61eaf825c99594be7de4dc
Razakhan15 <92670281+Razakhan15@users.noreply.github.com>
2024-08-14 18:45:40 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,45 @@
+## Features
+
+1. Drag and drop nodes.
+2. Connect nodes by clicking and dragging from one node's edge to another.
+3. Clicking on a node selects it, and its label can be modified from the sidebar.
+4. Clicking anywhere on the pane deselects the selected node.
+5. Save and restore functionality, which saves the current state of the nodes and their connections, and can restore the state later.
+5. Node can be deleted by clicking them and pressing backspace
+
+## Setup and Installation
+
+First, clone the repository to your local machine:
+
+```bash
+git clone https://github.com/sidx8/Chatbot-flow-builder.git
+```
+
+Navigate into the project directory:
+
+```bash
+cd [project-folder-name]
+```
+
+Install the project dependencies:
+
+```bash
+npm install
+```
+
+To start the project in development mode:
+
+```bash
+npm start
+```
+
+The application should now be running on http://localhost:3000.
+
+## Deployment
+
+The project can be deployed on any hosting service that supports Node.js applications. Examples of such services include Heroku, Vercel, and Netlify. Please refer to the respective platform's documentation for deploying React applications.
+
+
+## Support
+
+If you are having issues with the project, please let me know. i have an email, siddharth0sinha@gmail.com, where you can reach out to me.
---
+++ app/component/sidebar.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+export default function Sidebar({
+  nodeName,
+  setNodeName,
+  selectedNode,
+  setSelectedElements,
+}) {
+  const handleInputChange = (event) => {
+    setNodeName(event.target.value);
+  };
+  const onDragStart = (event, nodeType) => {
+    event.dataTransfer.setData("application/reactflow", nodeType);
+    event.dataTransfer.effectAllowed = "move";
+  };
+
+  return (
+    <>
+      {selectedNode && (
+        <div className="absolute top-1/4 left-1/4 ">
+          <div class=" bg-gray-200 rounded-lg shadow  flex-col">
+            <div class="flex-col items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+              <h3 className="text-xl mb-2 text-blue-900">Update Node</h3>
+              <label className="block mb-2 text-sm font-medium text-blue-900">
+                Node Name:
+              </label>
+              <input
+                type="text"
+                className="block w-full pt-2 px-3 pb-3 text-gray-700 border border-blue-300 rounded-lg bg-white focus:outline-none focus:border-blue-500"
+                value={nodeName}
+                onChange={handleInputChange}
+              />
+              <button
+                className="mt-4 bg-blue-500 text-white rounded p-2 hover:bg-blue-600"
+                onClick={() => setSelectedElements([])}
+              >
+                Go Back
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+      <aside className="border-r-2 border-blue-200 p-4 text-sm bg-blue-100 w-64 h-screen text-black">
+        <h3 className="text-xl mb-4 text-blue-900">Nodes Panel</h3>
+        <div
+          className="bg-white p-3 border-2 border-blue-500 rounded cursor-move flex justify-center items-center text-blue-500 hover:bg-blue-500 hover:text-white transition-colors duration-200"
+          onDragStart={(event) => onDragStart(event, "textnode")}
+          draggable
+        >
+          Message Node
+        </div>
+      </aside>
+    </>
+  );
+}
